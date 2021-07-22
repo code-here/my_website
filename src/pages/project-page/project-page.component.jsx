8a3af554ef8a1a58d3e4f8051f48a0c23abe2754
Abhinav Yadav <abhinavy14@gmail.com>
2021-07-22 22:41:30 +0530

    styled project page

--- src/pages/project-page/project-page.component.jsx
+++ src/pages/project-page/project-page.component.jsx
@@ -1,21 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
+
 import projects from "../projects/projects.data";
 
+import { ReactComponent as GithubIcon } from "../../assets/github.svg";
+import { ReactComponent as WebsiteIcon } from "../../assets/website.svg";
+
+import "./project-page.styles.scss";
+
 const ProjectPage = ({ match }) => {
-  let { title, description, github_link, website_link, tags } = projects.find(
-    (project) => project.id === parseInt(match.params.projectId)
-  );
+  let { title, description, github_link, website_link, tags, readme } =
+    projects.find((project) => project.id === parseInt(match.params.projectId));
+
+  let [markDown, setMarkDown] = useState("");
+  useEffect(() => {
+    let getMark = async () => {
+      let res = await fetch(readme);
+      let mark = await res.text();
+      setMarkDown(mark);
+    };
+    getMark();
+  }, [readme]);
+
   return (
     <div className="project-page">
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <p>
-        github:<a href={github_link}>{github_link}</a>
-      </p>
-      <p>
-        website:<a href={website_link}>{website_link}</a>
-      </p>
-      <p>{tags.map((tag) => `${tag}  `)}</p>
+      <h1 className="title">{title}</h1>
+      <div className="description">
+        <p>{description}</p>
+        <div className="link_container">
+          {github_link ? (
+            <a href={github_link}>
+              <GithubIcon />
+              Github
+            </a>
+          ) : (
+            ""
+          )}
+          {website_link ? (
+            <a href={website_link}>
+              <WebsiteIcon /> Website
+            </a>
+          ) : (
+            ""
+          )}
+        </div>
+        <p className="tags">
+          {tags.map((tag, idx) => (
+            <span key={idx}>{tag}</span>
+          ))}
+        </p>
+      </div>
+      <ReactMarkdown
+        className="markdown"
+        remarkPlugins={[gfm]}
+        children={markDown}
+      ></ReactMarkdown>
     </div>
   );
 };
--- src/pages/projects/projects.data.js
+++ src/pages/projects/projects.data.js
@@ -12,7 +12,8 @@ const projects = [
         description: "react app: fetch news from hacker stories api",
         technology: "react",
         tags: ["ReactJs", "Nodejs", "Web"],
-        type: "web"
+        type: "web",
+        readme: "https://raw.githubusercontent.com/code-here/hackerStories/main/README.md"
     },
     {
         id: 2,
@@ -22,7 +23,8 @@ const projects = [
         description: "nodejs app: fetches data from nasa's APOD api",
         technology: "nodejs",
         tags: ["NodeJs", "API", "NASA APOD"],
-        type: "web"
+        type: "web",
+        readme: "https://raw.githubusercontent.com/code-here/nasa-dailyFeedApp/main/README.md"
     },
     {
         id: 3,
@@ -32,7 +34,8 @@ const projects = [
         description: "rust app: cli app to fetch nasa astronomical pic of the day",
         technology: "rust",
         tags: ["Rust", "CLI", "NASA APOD"],
-        type: "cli"
+        type: "cli",
+        readme: "https://raw.githubusercontent.com/code-here/nasa_apod/main/README.md"
     },
     {
         id: 4,
@@ -42,7 +45,8 @@ const projects = [
         description: "rust app: convert csv to pretty tables",
         technology: "rust",
         tags: ["Rust", "CLI", "csv", "tables"],
-        type: "cli"
+        type: "cli",
+        readme: "https://raw.githubusercontent.com/code-here/tabulate/main/README.md"
     },
     {
         id: 5,
@@ -52,7 +56,8 @@ const projects = [
         description: "some sorting algorithms in rust",
         technology: "rust",
         tags: ["Rust", "CLI", "algorithms"],
-        type: "cli"
+        type: "cli",
+        readme: "https://raw.githubusercontent.com/code-here/sorting_algos/main/README.md"
     },
     {
         id: 6,
@@ -62,18 +67,65 @@ const projects = [
         description: "task manager REST api written in nodejs",
         technology: "nodejs",
         tags: ["Nodejs", "REST API", "MongoDB", "Mongoose", "Express"],
-        type: "web"
+        type: "web",
+        readme: "https://raw.githubusercontent.com/code-here/task-manager/main/README.md"
     },
     {
         id: 7,
         title: "Crown Clothing",
         github_link: `${ github_baseurl }/crwn_clothing`,
-        website_link: undefined,
+        website_link: "crwn-clothing-mart.herokuapp.com",
         description: "shopping web app in made with react, redux",
         technology: "react",
-        tags: ["Reactjs", "Redux", "SCSS"],
-        type: "web"
-    }
+        tags: ["Reactjs", "Redux", "SCSS", "responsive", "redux-saga", "firebase", "react-router", "sass", "nodejs", "express"],
+        type: "web",
+        readme: "https://raw.githubusercontent.com/code-here/crwn_clothing/main/README.md"
+    },
+    {
+        id: 8,
+        title: "REST API in rust",
+        github_link: `${ github_baseurl }/rust_api`,
+        website_link: undefined,
+        description: "a user management REST API made with actix-web and diesel",
+        technology: "rust",
+        tags: ["rust", "diesel", "actix-web", "rust-argon2", "actix-identity", "auth", "API", "Backend"],
+        type: "web",
+        readme: "https://raw.githubusercontent.com/code-here/rust_api/main/README.md"
+    },
+    {
+        id: 9,
+        title: "cat-rs",
+        github_link: `${ github_baseurl }/cat-rs`,
+        website_link: undefined,
+        description: "a simple implimentation of linux utility cat in rust",
+        technology: "rust",
+        tags: ["rust", "linux", "command"],
+        type: "cli",
+        readme: "https://raw.githubusercontent.com/code-here/cat-rs/main/README.md"
+    },
+    {
+        id: 10,
+        title: "Time normalizer",
+        github_link: `${ github_baseurl }/time-normalizer`,
+        website_link: undefined,
+        description: "a cli app which normalizes time",
+        technology: "rust",
+        tags: ["rust", "cli"],
+        type: "cli",
+        readme: "https://raw.githubusercontent.com/code-here/time-normalizer/main/README.md"
+    },
+    {
+        id: 11,
+        title: "My website",
+        github_link: `${ github_baseurl }/my_website`,
+        website_link: `${ website_baseurl }/my_website`,
+        description: "a website to showcase my projects",
+        technology: "react",
+        tags: ["reactjs", "website", "nodejs", "HTML", "sass"],
+        type: "web",
+        readme: "https://raw.githubusercontent.com/code-here/my_website/main/README.md"
+    },
+    
 ]
 
 export default projects;
