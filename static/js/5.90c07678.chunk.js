53a7d70f1c37da11453417ace184a88d1acaa0b6
Abhinav Yadav <abhinavy14@gmail.com>
2021-07-24 22:20:59 +0530

    Updates

---
+++ static/js/5.90c07678.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmy_website=this.webpackJsonpmy_website||[]).push([[5],{239:function(e,s,t){},242:function(e,s,t){"use strict";t.r(s);t(0),t(239);var i=t(3);s.default=function(){return Object(i.jsx)("div",{className:"footer",children:Object(i.jsx)("span",{children:" Made by Abhinavy14(0x40)gmail.com "})})}}}]);
+//# sourceMappingURL=5.90c07678.chunk.js.map
